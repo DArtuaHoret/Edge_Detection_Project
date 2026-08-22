faa5d39a07738c7373683dd63812832595689ae1
Anzhalika Kupryianava <anzhalika.kupryianava@gmail.com>
2026-08-22 17:05:22 +0200

    Update edge_fragment.glsl

--- shaders/edge_fragment.glsl
+++ shaders/edge_fragment.glsl
@@ -1,5 +1,5 @@
-""" Shader fragmentów wykrywania krawędzi metodą Sobela. Analizuje wartości głębokości i normalnych
-    w sąsiednich pikselach, wykrywa gwałtowne zmiany tych danych i tworzy biały kontur."""
+// Shader fragmentów wykrywania krawędzi metodą Sobela. Analizuje wartości głębokości i normalnych
+// w sąsiednich pikselach, wykrywa gwałtowne zmiany tych danych i tworzy biały kontur.
 
 #version 330 core
 
